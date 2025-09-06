26ef46b24ddb4300663741f0399954c5e9666dd0
theoutom <125547899+theoutom@users.noreply.github.com>
2025-09-06 08:31:19 +0700

    memasukkan semua input ke main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Mengimpor module utils.py
 import utils
 
-nilai_input = float(input("Masukkan nilai suhu yang akan anda konversi (misal 100): "))
 # Mencetak ucapan selamat datang
 print(utils.garis)
 print("Selamat datang di konversi suhu!")
 print(utils.garis)
+
+nilai_input = float(input("Masukkan nilai suhu yang akan anda konversi (misal 100): "))
+dari_input = input("Masukkan asal suhu anda (C/K/F): ").upper()
+ke_input = input("Masukkan tujuan suhu ands (C/K/F): ").upper()
+
