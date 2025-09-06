f2113a18218ec16242e55b06a1cd2d9bd068de98
theoutom <125547899+theoutom@users.noreply.github.com>
2025-09-06 08:19:22 +0700

    Memasukkan pendeklarasian variabel garis ke utils.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # Mengimpor module utils.py
 import utils
 
-# Mendeklarasikan garis untuk memisahkan proses yang sedang berjalan
-garis = '=' * 25
-garis2 = '-' * 25
-
 # Mencetak ucapan selamat datang
 print(garis)
 print("Selamat datang di konversi suhu!")
